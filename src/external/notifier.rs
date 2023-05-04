67ff87080a31998ffdf70909bbe2151467a8819d
Kryszak <prajskrzysztof@gmail.com>
2023-05-04 21:22:10 +0200

    handle notification send fail with logging error

--- src/external/notifier.rs
+++ src/external/notifier.rs
@@ -1,19 +1,26 @@
+use log::debug;
 use notify_rust::Notification;
 
 use crate::player::SelectedSongFile;
 
 pub fn notify_playback_start(song_metadata: &SelectedSongFile) {
-    Notification::new()
+    if Notification::new()
         .summary("Penny")
         .body(&song_metadata.display_short())
         .show()
-        .unwrap();
+        .is_err()
+    {
+        debug!("Failed to send playback start notification.");
+    }
 }
 
 pub fn notify_playback_stopped() {
-    Notification::new()
+    if Notification::new()
         .summary("Penny")
         .body("Stopped")
         .show()
-        .unwrap();
+        .is_err()
+    {
+        debug!("Failed to send playback stop notification.");
+    }
 }
